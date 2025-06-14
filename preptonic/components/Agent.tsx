a1548f1eb41dd99794e19dff9166967f2a40416b
PratikPanda <pratic.panda@gmail.com>
2025-06-14 17:15:44 +0530

    Added Agent Page for Interview

---
+++ preptonic/components/Agent.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import Image from 'next/image'
+import { cn } from '@/lib/utils';
+
+enum CallStatus {
+    INACTIVE = "INACTIVE",
+    CONNECTING = "CONNECTING",
+    ACTIVE = "ACTIVE",
+    FINISHED = "FINISHED",
+  }
+  
+
+const Agent = ({userName} : AgentProps) => {
+    const callStatus = CallStatus.INACTIVE;
+    const isSpeaking = true;
+    
+  return (
+    <>
+        <div className="call-view">
+        {/* AI Interviewer Card */}
+        <div className="card-interviewer">
+          <div className="avatar">
+            <Image
+              src="/ai-avatar.png"
+              alt="profile-image"
+              width={65}
+              height={54}
+              className="object-cover"
+            />
+            {isSpeaking && <span className="animate-speak" />}
+          </div>
+          <h3>AI Interviewer</h3>
+        </div>
+
+        {/* User Profile Card */}
+        <div className="card-border">
+          <div className="card-content">
+            <Image
+              src="/user-avatar.png"
+              alt="profile-image"
+              width={539}
+              height={539}
+              className="rounded-full object-cover size-[120px]"
+            />
+            <h3>{userName}</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full flex justify-center">
+        {callStatus !== "ACTIVE" ? (
+          <button className="relative btn-call">
+            <span
+              className={cn(
+                "absolute animate-ping rounded-full opacity-75",
+                callStatus !== "CONNECTING" && "hidden"
+              )}
+            />
+
+            <span className="relative">
+              {callStatus === "INACTIVE" || callStatus === "FINISHED"
+                ? "Call"
+                : ". . ."}
+            </span>
+          </button>
+        ) : (
+          <button className="btn-disconnect">
+            End
+          </button>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Agent
