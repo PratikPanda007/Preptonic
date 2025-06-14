cc66e08649a940b7eb549392e6fc9ee8dca2b54d
PratikPanda <pratic.panda@gmail.com>
2025-06-14 17:38:30 +0530

    Changes in Agent page, implemented Interview Generation Script texts.

--- preptonic/components/Agent.tsx
+++ preptonic/components/Agent.tsx
@@ -13,6 +13,13 @@ enum CallStatus {
 const Agent = ({userName} : AgentProps) => {
     const callStatus = CallStatus.INACTIVE;
     const isSpeaking = true;
+
+    const messages = [
+      'What\'s your Name',
+      'My Name is John Doe, Nice to mee you'
+    ];
+
+    const lastMessage = messages[messages.length-1]
     
   return (
     <>
@@ -46,6 +53,16 @@ const Agent = ({userName} : AgentProps) => {
           </div>
         </div>
       </div>
+
+      {messages.length > 0 && (
+        <div className='transcript-border'>
+          <div className='transcript'>
+            <p key={lastMessage} className={cn('transition-opacity dutation-500 opacity-0', 'animate-fadeIn opacity-100')}>
+              {lastMessage}
+            </p>
+          </div>
+        </div>
+      )}
 
       <div className="w-full flex justify-center">
         {callStatus !== "ACTIVE" ? (
